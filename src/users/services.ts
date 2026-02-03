53ae1942deaf70fcd83cf1138196aeb52e466c64
jesus rodriguez <jesus.dev.rodriguez@gmail.com>
2026-02-03 00:09:01 -0300

    feat(users): agregar users services
    - servicios para obtener usuarios
    - servicios para agregar usuarios

---
+++ src/users/services.ts
@@ -0,0 +1,7 @@
+let users = [
+    { id: 1, name: 'Juan Perez', email: 'juan@example.com' },
+    { id: 2, name: 'Maria Garcia', email: 'maria@example.com' },
+];
+
+export const getUsers = () => users;
+export const createUser = (user: any) => users.push({ ...user, id: users.length + 1 });
